996c4044c8c02beb0cf5ff5c9b6988381db18675
neurify <michael@neurify.co.uk>
2022-03-19 12:44:47 +0000

    work beginning on preferences page

--- Mother-Message/MainViewController.swift
+++ Mother-Message/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainViewController: UIViewController {
     
@@ -19,11 +20,23 @@ class MainViewController: UIViewController {
     
     // MARK: - Variables
     var dailyMessage: String?
+    
+    // MARK: - Instances
+    let db = Firestore.firestore()
+    let decoder = JSONDecoder()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        downloadDailyMessage()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dailyMessage = "This is sexy af"
+        
+        styleView()
     }
  
     // MARK: - IBActions
@@ -40,6 +53,16 @@ class MainViewController: UIViewController {
         
     }
     
+    func styleView() {
+        messageBackgroundView.layer.cornerRadius = messageBackgroundView.bounds.height/8
+        messageBackgroundView.layer.cornerCurve = .continuous
+        messageBackgroundView.layer.borderWidth = 2
+        messageBackgroundView.layer.borderColor = CGColor(red: 184/255, green: 70/255, blue: 246/255, alpha: 1)
+        
+        sendButtonView.layer.cornerRadius = sendButtonView.bounds.height/2
+        sendButtonView.layer.cornerCurve = .continuous
+    }
+    
     func sendMessageToWhatsapp() {
         guard let dailyMessage = dailyMessage else {
             return
@@ -52,5 +75,39 @@ class MainViewController: UIViewController {
         }
     }
     
+    func downloadDailyMessage() {
+        
+        db.collection("dailyMessages").document("dailyMessage").getDocument { snapshot, error in
+            if let error = error {
+                print("error", error.localizedDescription)
+                return
+            }
+            
+            guard let document = snapshot?.data() else {
+                print("error: \(#line)")
+                return
+            }
+            
+            self.parseDailyMessage(message: document)
+        }
+        
+    }
+    
+    func parseDailyMessage(message: [String: Any]) {
+        
+        do {
+            let convertedJSON = try JSONSerialization.data(withJSONObject: message, options: [])
+            let parsedJSON = try decoder.decode(DailyMessage.self, from: convertedJSON)
+            dailyMessage = parsedJSON.messageText
+            messageTextView.text = parsedJSON.messageText
+        } catch {
+            print("error parsing")
+        }
+        
+    }
+    
+}
 
+struct DailyMessage: Codable {
+    var messageText: String?
 }
--- Mother-Message/PreferencesViewController.swift
+++ Mother-Message/PreferencesViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class PreferencesViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var preferencesLabel: UILabel!
+    @IBOutlet weak var preferencesStackView: UIStackView!
+    @IBOutlet weak var addNumberButtonView: UIView!
+    @IBOutlet weak var addNumberButton: UIButton!
+    @IBOutlet weak var allowNotifsButtonView: UIView!
+    @IBOutlet weak var allowNotifsButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-   
+    
+    @IBAction func addNumberTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func allowNotifsTapped(_ sender: Any) {
+    }
+    
 
 }
